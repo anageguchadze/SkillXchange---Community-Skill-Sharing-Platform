5bad10f44eec5185bc3baab3af0ea47fb4c84a2b
Ana Geguchadze <anageguchadze22@gmail.com>
2024-10-21 16:04:52 +0200

    Add yasg.py file to organize Swagger schema view configuration

--- skillxchange_app/urls.py
+++ skillxchange_app/urls.py
@@ -1,13 +1,16 @@
+# skillxchange_app/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet, CourseViewSet, EnrollmentViewSet, ReviewViewSet
 
+# Create a router and register your viewsets
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet)
 router.register(r'courses', CourseViewSet)
 router.register(r'enrollments', EnrollmentViewSet)
 router.register(r'reviews', ReviewViewSet)
 
+# Define your URL patterns
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),  # This will include all the routes created by the router
 ]
--- skillxchange_app/views.py
+++ skillxchange_app/views.py
@@ -1,15 +1,33 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from .models import Course, CustomUser, Enrollment, Review
 from .serializers import CustomUserSerializer, CourseSerializer, EnrollmentSerializer, ReviewSerializer
+from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
-class CustomUserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
+
+class CourseFilter(FilterSet):
+    category = filters.CharFilter(field_name='category', lookup_expr='icontains')
+    max_students = filters.NumberFilter(field_name='max_students')
+
+    class Meta:
+        model = Course
+        fields = ['category', 'max_students']
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = CourseFilter
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+# class CourseViewSet(viewsets.ModelViewSet):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
 
 class EnrollmentViewSet(viewsets.ModelViewSet):
     queryset = Enrollment.objects.all()
@@ -18,3 +36,8 @@ class EnrollmentViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+class CustomPagination(PageNumberPagination):
+    page_size = 5  
+    page_size_query_param = 'page_size'  
+    max_page_size = 100
